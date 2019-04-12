5b4de81715b0b69c82a46ecedaa0ce76bd8536ea
John Kartupelis <john.kartupelis@experian.com>
2019-04-12 19:00:08 +0100

    Initial Commit

---
+++ Stabby/Stabby.h
@@ -0,0 +1,18 @@
+//
+//  Stabby.h
+//  Stabby
+//
+//  Copyright © 2019 John Kartupelis. All rights reserved.
+// 
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stabby.
+FOUNDATION_EXPORT double StabbyVersionNumber;
+
+//! Project version string for Stabby.
+FOUNDATION_EXPORT const unsigned char StabbyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stabby/PublicHeader.h>
+
+
